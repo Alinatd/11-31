76e6b465c72838d3683d0c37225d27499be37f6f
Alina Turdakunova <141915076+Alinatd@users.noreply.github.com>
2023-11-24 23:06:01 +0600

    Add files via upload

---
+++ just.js
@@ -0,0 +1,16 @@
+let res = document.getElementById('res')
+let input1 = document.getElementById('input1')
+let input2 = document.getElementById('input2')
+let plus = document.querySelector('knop+')
+let minus = document.querySelector("knop-")
+let um = document.querySelector("knop*")
+let del = document.querySelector("knop/")
+let per = document.querySelector("knop%")
+
+
+function plus(){
+input1.value +input2.value
+}
+function res(){
+    
+}
